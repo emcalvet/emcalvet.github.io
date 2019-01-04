31111a291430ef2a47c2f2a7884a6ebb78c5e017
Emma Calvet <emma.calvet.g@gmail.com>
2019-01-04 22:27:56 +0100

    ajout js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+Voici le BetterMotherFuckingWebsite avec sa pop up
 Voir la version en ligne : http://emcalvet.github.io
---
+++ scripts.js
@@ -0,0 +1,5 @@
+let popUp = prompt('Voulez-vous aller sur xxxvidsxxx ? Tapez "oui"');
+
+if (popUp === 'oui') {
+   window.location.href= 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+} 
